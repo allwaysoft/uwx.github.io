2a196692680bac53944c5831ad1626462d13e54e
chrishansen69 <cadrekucra@gmx.com>
2016-09-23 20:39:58 -0300

    hansen renderer using markdown-it built-in

--- local_modules/hexo-renderer-hansen/lib/renderer.js
+++ local_modules/hexo-renderer-hansen/lib/renderer.js
@@ -1,7 +1,16 @@
 'use strict';
 /*jshint node:true*/
+
+const hrender = require('./hrenderer.js');
+
 module.exports = function (data, options) {
-  return data.text.replace(/:::( (\w+)(\r\n|\n))?(.*?):::/g, function(match, $1, spoiler_ticker, $3, spoiler_content) {
-    return `<div class="spoiler-parent"><dt class="spoiler-text"><strong>` + (spoiler_ticker || 'Spoiler:') + `</strong><a href="javascript:void(0);" class="spoiler-toggle">Show</a></dt><dd><div class="spoiler-content" name="spoiler" style="display: none;">` + spoiler_content + `</div></dd></div>`;
+  data.text = data.text.replace(/::: ([\s\S]*?)(\r\n|\n)([\s\S]*?):::/g, function(match, spoiler_ticker, $2, spoiler_content) {
+    console.log('its replacin alright');
+    return `<div class="spoiler-parent"><dt class="spoiler-text"><strong>` + spoiler_ticker + `</strong><a href="javascript:void(0);" class="spoiler-toggle">Show</a></dt><dd><div class="spoiler-content" name="spoiler" style="display: none;">` + spoiler_content + `</div></dd></div>`;
+  }).replace(/:::([\s\S]*?):::/g, function(match, spoiler_content) {
+    console.log('its replacin alright');
+    return `<div class="spoiler-parent"><dt class="spoiler-text"><strong>Spoiler: </strong><a href="javascript:void(0);" class="spoiler-toggle">Show</a></dt><dd><div class="spoiler-content" name="spoiler" style="display: none;">` + spoiler_content + `</div></dd></div>`;
   });
+
+  return hrender.call(this, data, options);
 };
