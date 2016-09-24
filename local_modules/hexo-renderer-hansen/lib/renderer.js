5ed6a2f814ef71e40f0ecfd18be90de5a1cc1661
chrishansen69 <cadrekucra@gmx.com>
2016-09-23 22:59:33 -0300

    add todo to renderer

--- local_modules/hexo-renderer-hansen/lib/renderer.js
+++ local_modules/hexo-renderer-hansen/lib/renderer.js
@@ -3,7 +3,7 @@
 
 const hrender = require('./hrenderer.js');
 
-module.exports = function (data, options) {
+module.exports = function (data, options) { // TODO: possibly add 2 empty lines after each spoiler content start and end so markdown-it handles it properly
   data.text = data.text.replace(/::: ([\s\S]*?)(\r\n|\n)([\s\S]*?):::/g, function(match, spoiler_ticker, $2, spoiler_content) {
     console.log('its replacin alright');
     return `<div class="spoiler-parent"><dt class="spoiler-text"><strong>` + spoiler_ticker + `</strong><a href="javascript:void(0);" class="spoiler-toggle">Show</a></dt><dd><div class="spoiler-content" name="spoiler" style="display: none;">` + spoiler_content + `</div></dd></div>`;
