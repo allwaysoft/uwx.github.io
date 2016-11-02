1a93cbf5cb529147a4e9f11f7a31a4dadebd717c
chrishansen69 <cadrekucra@gmx.com>
2016-11-01 22:33:19 -0200

    build

---
+++ libraries/common.js
@@ -0,0 +1,31 @@
+// common hansen funcs used across tool files
+'use strict';
+/* jshint browser:true */
+/* exported makeFunc */
+/* globals console, myCodeMirror */
+
+function makeFunc(action, errorText) {
+  return function() {
+    try {
+      myCodeMirror.setValue(action(myCodeMirror.getValue()));
+    } catch (err) {
+      outputText(errorText + err);
+      console.error(err);
+      console.trace(err);
+    }
+  };
+}
+
+function makeVisible(el) {
+  el.style.display = '';
+}
+
+//  function toggleSelector(selector) {
+//    toggleElement(document.querySelector(selector));
+//  }
+
+function outputText(str) {
+  var lintOutput = document.querySelector('#lint-output');
+  makeVisible(lintOutput);
+  lintOutput.innerHTML = str.replace(/\n/g, '<br>');
+}
