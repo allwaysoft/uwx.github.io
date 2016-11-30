41e8269516bfab403da5ee0553cc79c9f30e8e4e
chrishansen69 <cadrekucra@gmx.com>
2016-11-30 14:25:37 -0200

    new ci publish

---
+++ ci-publish-new.cmd
@@ -0,0 +1,21 @@
+@rem set git data
+git config --global user.email "bbbbbbenes@outlook.com.br"
+git config --global user.name "Appveyor Automated Build"
+
+@rem set date
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+
+@rem clone gh pages branch
+mkdir %USERPROFILE%\master
+git clone --quiet --branch=master https://github.com/rafa1231518/OpenNFMM %USERPROFILE%\master
+
+@rem override public data and add to git
+copy /y public %USERPROFILE%\master
+cd %USERPROFILE%\master
+dir
+git add -f .
+
+@rem commit it
+git commit -m "Built page at %mydate%_%mytime% [skip ci]"
+git push origin master
