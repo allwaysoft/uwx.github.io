cd5a2fbebe3cf2cfc75aad7a3fa2f2694e6a6adf
uwx <bbbbbbenes@outlook.com.br>
2018-10-11 04:54:17 -0300

    update repo path

--- ci-publish-new.cmd
+++ ci-publish-new.cmd
@@ -1,5 +1,5 @@
 @rem set git data
-git config --global user.email "bbbbbbenes@outlook.com.br"
+git config --global user.email "travis@ci.domain"
 git config --global user.name "Appveyor Automated Build"
 
 @rem set date
@@ -8,7 +8,7 @@ For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
 @rem clone gh pages branch
 mkdir %USERPROFILE%\master
-git clone --quiet --branch=master https://github.com/fallk/fallk.github.io %USERPROFILE%\master
+git clone --quiet --branch=master https://github.com/uwx/uwx.github.io %USERPROFILE%\master
 
 @rem override public data and add to git
 robocopy public %USERPROFILE%\master /E /IS /NFL /NP
